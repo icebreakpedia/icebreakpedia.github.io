b9b3ffc6fb5f3ec18142450bdb761f73ebebb2de
6add2 <lukkakit62@gmail.com>
2025-07-10 18:32:18 +0800

    .

--- utility.js
+++ utility.js
@@ -48,7 +48,6 @@ async function scanQRCode() {
                     video.style.display = 'none';
                     document.getElementById('stopCamera').style.display = 'none';
                     document.getElementById('inputRoomNumber').value = roomNumber;
-                    requestFullScreen();
                     showInput();
                     setupButtonHandlers(roomNumber);
                 } else {
